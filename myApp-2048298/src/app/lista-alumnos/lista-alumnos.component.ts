9453ee3c7e15ff0f6c1efebedfb8b7dc1f420f7f
AlainLobato <lobatoa829@gmail.com>
2024-03-25 15:40:42 -0600

    Practica 8

--- myApp-2048298/src/app/lista-alumnos/lista-alumnos.component.ts
+++ myApp-2048298/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-lista-alumnos',
@@ -7,12 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaAlumnosComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private actionSheetCtrl: ActionSheetController){ }
 
   ngOnInit() {}
 
+  alumnos : any = ["Carlos", "Noe", "Alain", "Karla", "Barbara", "Regina", "Valeria", "Ailton", "Simone"];
 
-  alumnos : any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+  async favorite(){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Options',
+      buttons: [
+        {
+          text: 'Add to Favorites',
+          data: {
+            action: 'share',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
 
+    await actionSheet.present();
+  }
+
+  async delete(){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Options',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
   
 }
